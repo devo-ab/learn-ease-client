e2a43a998a4244ca4194b343c1afc59001f0f8ed
designer-ab <per.useravi0@gmail.com>
2024-06-01 23:19:09 +0600

    signup from added with responsive without functionality

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,12 +1,19 @@
 import { createBrowserRouter } from "react-router-dom";
 import Layouts from "../Layouts/Layouts";
 import ErrorElement from "../ErrorElement/ErrorElement";
+import SignUp from "../Pages/SignUp/SignUp";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layouts></Layouts>,
-    errorElement: <ErrorElement></ErrorElement>
+    errorElement: <ErrorElement></ErrorElement>,
+    children: [
+        {
+            path: "sign-up",
+            element: <SignUp></SignUp>
+        }
+    ]
   },
 ]);
 
