6ee9a2205ee6763c80441f9bfa7509dff553f11d
designer-ab <per.useravi0@gmail.com>
2024-06-01 20:08:29 +0600

    added error page with responsive

--- src/ErrorElement/ErrorElement.jsx
+++ src/ErrorElement/ErrorElement.jsx
@@ -1,9 +1,19 @@
-const ErrorElemrnt = () => {
-    return (
-        <div>
-            <h1>Error</h1>
-        </div>
-    );
+import { Link } from "react-router-dom";
+import error404 from "../assets/404.gif";
+
+const ErrorElement = () => {
+  return (
+    <div className="max-w-md md:max-w-2xl mx-auto">
+      <img className="" src={error404} alt="" />
+      <div className="text-center">
+        <p className="text-2xl font-semibold md:text-3xl">Sorry, we couldn't find this page.</p>
+        <p className="mt-4 mb-8">
+          But dont worry, you can find plenty of other things on our homepage.
+        </p>
+      </div>
+      <Link to="/"><button className="bg-[#e67e22] text-white px-4 py-2 rounded flex mx-auto">Back to Home</button></Link>
+    </div>
+  );
 };
 
-export default ErrorElemrnt;
+export default ErrorElement;
--- src/Layouts/Layouts.jsx
+++ src/Layouts/Layouts.jsx
@@ -4,7 +4,7 @@ import Footer from "../Components/Footer/Footer";
 
 const Layouts = () => {
     return (
-        <div>
+        <div className="max-w-7xl mx-auto px-4 lg:px-0">
             <Navbar></Navbar>
             <Outlet></Outlet>
             <Footer></Footer>
