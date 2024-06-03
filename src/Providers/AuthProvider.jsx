8cb1365b0bdc97d7fb5309562d8881667488dce7
designer-ab <per.useravi0@gmail.com>
2024-06-03 20:57:39 +0600

    get token and save it

--- src/Providers/AuthProvider.jsx
+++ src/Providers/AuthProvider.jsx
@@ -11,6 +11,7 @@ import {
   updateProfile,
 } from "firebase/auth";
 import app from "../Firebase/firebase.config";
+import useAxiosPublic from "../Hooks/useAxiosPublic";
 
 export const AuthContext = createContext(null);
 const auth = getAuth(app);
@@ -22,6 +23,7 @@ const AuthProvider = ({ children }) => {
   // prodiver
 
   const googleProvider = new GoogleAuthProvider();
+  const axiosPublic = useAxiosPublic();
 
   // prodiver
 
@@ -56,16 +58,35 @@ const AuthProvider = ({ children }) => {
 
   // signUp signIn signIn Out
 
-  useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
-      console.log("user in the auth state changed", currentUser);
-      setUser(currentUser);
-      setLoading(false);
+  useEffect( () => {
+    const unsubscribe = onAuthStateChanged(auth, currentUser => {
+        setUser(currentUser);
+        console.log('current user', currentUser);
+        if(currentUser){
+            const userInfo = {email : currentUser.email}
+            //get token and store client
+            axiosPublic.post('/jwt', userInfo)
+            .then(res => {
+                // console.log(res.data)
+                if(res.data.token){
+                    localStorage.setItem('access-token', res.data.token)
+                }
+                setLoading(false);
+            })
+            .catch(err => {
+                console.log(err)
+            })
+        }
+        else{
+            // TODO : Remove token(if token store in the client side : Local Storage, chasing, in memory)
+            localStorage.removeItem('access-token')
+            setLoading(false);
+        }
     });
     return () => {
-      return unsubscribe();
-    };
-  }, []);
+        return unsubscribe();
+    }
+} ,[axiosPublic])
 
   const authInfo = { user, loading, signUpUser, signInUser, googleSignInUser, updateUserProfile, signOutUser };
 
