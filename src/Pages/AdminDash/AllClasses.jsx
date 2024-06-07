6902f026ae2c69430b8bd4149c67e71ae31cae3f
designer-ab <per.useravi0@gmail.com>
2024-06-08 00:16:01 +0600

    added all classes admin page with responsive

--- src/Pages/AdminDash/AllClasses.jsx
+++ src/Pages/AdminDash/AllClasses.jsx
@@ -1,5 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
 import useAxiosSecure from "../../Hooks/useAxiosSecure";
+import Swal from "sweetalert2";
 
 const AllClasses = () => {
   const axiosSecure = useAxiosSecure();
@@ -11,6 +12,62 @@ const AllClasses = () => {
       return res.data;
     },
   });
+
+  const handleApproved = (id) => {
+    const status = "approved";
+    const info = { status };
+    Swal.fire({
+      title: "Are you sure?",
+      text: "Do you want to approve this class?",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, approve!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        axiosSecure.patch(`/classtatus/${id}`, info)
+        .then((res) => {
+          if (res.data.modifiedCount > 0){
+            refetch();
+            Swal.fire({
+              title: "Approved",
+              text: "Class approved successfully.",
+              icon: "success",
+            });
+          }
+        });
+      }
+    });
+  };
+
+  const handleRejected = (id) => {
+    const status = "rejected";
+    const info = { status };
+    Swal.fire({
+      title: "Are you sure?",
+      text: "Do you want to reject this class?",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, reject!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        axiosSecure.patch(`/classtatus/${id}`, info)
+        .then((res) => {
+          if (res.data.modifiedCount > 0){
+            refetch();
+            Swal.fire({
+              title: "Rejected",
+              text: "Class Rejected successfully.",
+              icon: "success",
+            });
+          }
+        });
+      }
+    });
+  };
 
   return (
     <div>
@@ -52,19 +109,41 @@ const AllClasses = () => {
                 <td>{item.email}</td>
                 <td>{item.shortDes}</td>
                 <td>
-                  <button className="btn btn-ghost bg-green-500 text-white hover:text-black">
+                  {
+                    item.status === "approved" ? <button disabled
+                    className="btn  btn-ghost bg-green-500 text-white hover:text-black"
+                  >
+                    Approve
+                  </button> : <button
+                    onClick={() => handleApproved(item._id)}
+                    className="btn btn-ghost bg-green-500 text-white hover:text-black"
+                  >
                     Approve
                   </button>
+                  }
                 </td>
                 <td>
-                  <button className="btn btn-ghost bg-red-500 text-white hover:text-black">
+                  {
+                    item.status === "rejected" ? <button disabled
+                    className="btn btn-ghost bg-red-500 text-white hover:text-black"
+                  >
                     Reject
-                  </button>
+                  </button> : <button
+                  onClick={() => handleRejected(item._id)}
+                  className="btn btn-ghost bg-red-500 text-white hover:text-black"
+                >
+                  Reject
+                </button>
+                  }
                 </td>
                 <th>
-                  <button className="btn btn-ghost bg-orange-600 text-white hover:text-black">
+                  {
+                    item.status === "approved" ? <button className="btn btn-ghost bg-orange-600 text-white hover:text-black">
+                    Progress
+                  </button> : <button disabled className="btn btn-ghost bg-orange-600 text-white hover:text-black">
                     Progress
                   </button>
+                  }
                 </th>
               </tr>
             ))}
