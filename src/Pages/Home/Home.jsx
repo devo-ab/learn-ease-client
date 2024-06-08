ed120a7f8893e4cea48e5f2795af14c30ea988d2
designer-ab <per.useravi0@gmail.com>
2024-06-09 03:48:40 +0600

    added highlighted class with responsive

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,3 +1,4 @@
+import HighlightClass from "../../Components/HighlightClass/HighlightClass";
 import HomeBanner from "../../Components/HomeBanner/HomeBanner";
 import NewsLetter from "../../Components/NewsLetter/NewsLetter";
 import Offer from "../../Components/Offer/Offer";
@@ -9,6 +10,7 @@ const Home = () => {
     return (
         <div>
             <HomeBanner></HomeBanner>
+            <HighlightClass></HighlightClass>
             <TeacherSection></TeacherSection>
             <Offer></Offer>
             <OurTeam></OurTeam>
