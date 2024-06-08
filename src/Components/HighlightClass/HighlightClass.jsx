99631f3779a00b80534d46348a1f2bf02730b96c
designer-ab <per.useravi0@gmail.com>
2024-06-09 04:16:55 +0600

    added slider in highlighted class

--- src/Components/HighlightClass/HighlightClass.jsx
+++ src/Components/HighlightClass/HighlightClass.jsx
@@ -2,6 +2,19 @@ import { useQuery } from "@tanstack/react-query";
 import useAxiosPublic from "../../Hooks/useAxiosPublic";
 import { Link } from "react-router-dom";
 
+// react
+import React, { useRef, useState } from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
+import "swiper/css/pagination";
+
+// import './styles.css';
+
+// import required modules
+import { Pagination } from "swiper/modules";
+// react
 
 const HighlightClass = () => {
   const axiosPublic = useAxiosPublic();
@@ -21,7 +34,7 @@ const HighlightClass = () => {
         <p className="dark:text-gray-600">Our Most Popular and Impactful Courses</p>
       </div>
 
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+      {/* <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
         {highClass.map((item) => (
           <div
             key={item._id}
@@ -43,7 +56,42 @@ const HighlightClass = () => {
             <Link to={`/public-class-details/${item._id}`}><button className="btn btn-ghost btn-sm bg-[#e67e22] text-white hover:text-black mt-2 w-full">Enroll</button></Link>
           </div>
         ))}
-      </div>
+      </div> */}
+
+      <Swiper
+        slidesPerView={"auto"}
+        spaceBetween={30}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Pagination]}
+        className="mySwiper"
+      >
+        {highClass.map((item) => (
+          <SwiperSlide className="justify-center max-w-2xl mx-auto " key={item._id}>
+            <div className="border border-gray-700 bg-orange-100 rounded-sm p-4 mt-5 items-center justify-center max-w-2xl">
+              <div>
+                <img className="w-full max-h-52 rounded mx-auto" src={item.image} alt="" />
+              </div>
+              <div className="mt-2">
+                <h3 className="text-2xl font-semibold">Title : {item.title}</h3>
+                <div className="text-lg">
+                  <p>Educator : {item.name}</p>
+                </div>
+                <div>
+                  <p>Price : ${item.price}</p>
+                </div>
+                <p>Short Description : {item.shortDes}</p>
+              </div>
+              <Link to={`/public-class-details/${item._id}`}>
+                <button className="btn btn-ghost btn-sm bg-[#e67e22] text-white hover:text-black mt-2 w-full">
+                  Enroll
+                </button>
+              </Link>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </div>
   );
 };
